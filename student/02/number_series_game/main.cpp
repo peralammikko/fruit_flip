272151dcffd83ffd4b94604763cc7adff57049b1
Mikko Perälä <mikko.perala@tuni.fi>
2020-09-02 15:04:51 +0300

    lisätty 7:llä jaollisten käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -9,11 +9,16 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     cin >> luku;
     for (int i = 1; i <= luku; ++i) {
-        if (i % 3 == 0)
+        if ((i % 3 == 0 ) and (i % 7 == 0))
+                cout << "zip boing" << endl;
+        else if (i % 3 == 0)
             cout << "zip" << endl;
+        else if (i % 7 == 0)
+            cout << "boing" << endl;
         else {
             cout << i << endl;
         }
     }
+    return 0;
 
 }
