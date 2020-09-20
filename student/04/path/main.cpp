4e2bf3c20bc8db363ba197f1daeb434513f7e24b
Mikko Perälä <mikko.perala@tuni.fi>
2020-09-20 16:28:50 +0300

    Polku syötteet + liikkuttelu ilman tarkistusta

--- student/04/path/main.cpp
+++ student/04/path/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -37,8 +38,63 @@ void print(const vector<vector<char>>& g)
     cout << "===============" << endl;
 }
 
+bool tarkista_polku(vector<unsigned int> siirrot, vector<vector<char>>& g) {
+    int x1 = siirrot[2]-1;
+    int y1 = siirrot[3]-1;
+    int x0 = siirrot[0]-1;
+    int y0 = siirrot[1]-1;
+
+    int distancey = y1-y0;
+    int absy = abs(distancey);
+    int distancex = x1-x0;
+    int absx = abs(distancex);
+    int risteysx = 1;
+    int risteysy = 0;
+
+    if (absy == 0) {
+        while (x0 != x1) {
+        if (g[y1][x1] == 'o') {
+            if (x1 > x0)
+                x1 -= 1;
+            else if (x0 > x1)
+                x1 += 1;
+        } else if (g[y0][x0] == 'R' or g[y0][x0] == 'G') {
+            cout << CANNOT_MOVE << endl;
+            return false;
+        }
+        }
+    } else {
+        return true;
+    }
+
+    return true;
+}
+
+void move_piece(vector<unsigned int> siirrot, vector<vector<char>>& g) {
+    vector<unsigned int> alku = {siirrot[0]-1, siirrot[1]-1};
+    vector<unsigned int> loppu = {siirrot[2]-1, siirrot[3]-1};
+
+    switch(g.at(loppu[1]).at(loppu[0]))
+        {
+        case ' ': cout << INVALID_POINT << endl; return;
+        case 'R': cout << INVALID_POINT << endl; return;
+        case 'G': cout << INVALID_POINT << endl; return;
+        }
+
+    switch(g.at(alku[1]).at(alku[0]))
+        {
+        case ' ': cout << INVALID_POINT << endl; return;
+        case 'o': cout << INVALID_POINT << endl; return;
+        case 'G': if (tarkista_polku(siirrot, g) == true) {
+                    g.at(loppu[1]).at(loppu[0]) = 'G'; g.at(alku[1]).at(alku[0]) = 'o'; }; return;
+        case 'R': if (tarkista_polku(siirrot, g) == true) {
+                    g.at(loppu[1]).at(loppu[0]) = 'R'; g.at(alku[1]).at(alku[0]) = 'o'; }; return;
+        }
+    }
+
+
 vector<vector<char>> initBoard() {
-    vector<vector<char>> lauta = {{}, {}, {}, {}, {}, {}};
+    vector<vector<char>> lauta = {{}, {}, {}, {}, {}};
     lauta.at(0) = {'G', 'G', 'G', 'G'};
     lauta.at(1) = {' ', 'o', ' ', ' '};
     lauta.at(2) = {' ', 'o', 'o', ' '};
@@ -107,7 +163,9 @@ int main()
         cout << INPUT_TEXT;
         getline(cin, komento);
         vector<unsigned int> siirrot = split(komento);
+        move_piece(siirrot, grid);
     }
+
     return 0;
 
 }
