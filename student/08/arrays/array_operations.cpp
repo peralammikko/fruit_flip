bf178b78fb1642cb85536327d3fa66ee4356b94c
Mikko Perälä <mikko.perala@tuni.fi>
2020-11-01 16:51:05 +0200

    8.4.1 Arrays valmis

---
+++ student/08/arrays/array_operations.cpp
@@ -0,0 +1,52 @@
+#include "array_operations.hh"
+#include <iostream>
+
+
+
+using namespace std;
+
+int greatest_v1(int *itemptr, int size)
+{
+    int greatest = 0;
+    while (*itemptr < size) {
+        if (*itemptr > greatest) {
+            greatest = *itemptr;
+        }
+        itemptr++;
+    }
+    return greatest;
+}
+
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int greatest = 0;
+    while (itemptr < endptr) {
+        if (*itemptr > greatest) {
+            greatest = *itemptr;
+        }
+        itemptr++;
+    }
+    return greatest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    while (itemptr < endptr) {
+        *targetptr = *itemptr;
+        itemptr++;
+        targetptr++;
+    }
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+    while (leftptr < rightptr) {
+        int tempptr = *leftptr;
+        rightptr--;
+        *leftptr = *rightptr;
+        *rightptr = tempptr;
+        leftptr++;
+
+    }
+}
