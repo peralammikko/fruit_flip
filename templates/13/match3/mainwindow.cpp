aac2901c6ab6f55f928ead849b49ccedb0aed89c
Maarit Harsu <maarit.harsu@tuni.fi>
2020-11-18 12:04:09 +0200

    kierros 13 julkaistu / round 13 published

---
+++ templates/13/match3/mainwindow.cpp
@@ -0,0 +1,98 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+#include <QKeyEvent>
+#include <QDebug>
+#include <QPixmap>
+#include <vector>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // We need a graphics scene in which to draw rectangles
+    scene_ = new QGraphicsScene(this);
+
+    // The width of the graphicsView is BORDER_RIGHT added by 2,
+    // since the borders take one pixel on each side
+    // (1 on the left, and 1 on the right).
+    // Similarly, the height of the graphicsView is BORDER_DOWN added by 2.
+    ui->graphicsView->setGeometry(LEFT_MARGIN, TOP_MARGIN,
+                                  BORDER_RIGHT + 2, BORDER_DOWN + 2);
+    ui->graphicsView->setScene(scene_);
+
+    // The width of the scene_ is BORDER_RIGHT decreased by 1 and
+    // the height of it is BORDER_DOWN decreased by 1, because
+    // each square of a fruit is considered to be inside the sceneRect,
+    // if its upper left corner is inside the sceneRect.
+    scene_->setSceneRect(0, 0, BORDER_RIGHT - 1, BORDER_DOWN - 1);
+
+    int seed = time(0); // You can change seed value for testing purposes
+    randomEng_.seed(seed);
+    distr_ = std::uniform_int_distribution<int>(0, NUMBER_OF_FRUITS - 1);
+    distr_(randomEng_); // Wiping out the first random number (which is almost always 0)
+
+    init_titles();
+
+    // More code perhaps needed
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::init_titles()
+{
+    // Displaying column titles, starting from A
+    for(int i = 0, letter = 'A'; i < COLUMNS; ++i, ++letter)
+    {
+        int shift = 5;
+        QString letter_string = "";
+        letter_string.append(letter);
+        QLabel* label = new QLabel(letter_string, this);
+        label->setGeometry(LEFT_MARGIN + shift + i * SQUARE_SIDE,
+                           TOP_MARGIN - SQUARE_SIDE,
+                           SQUARE_SIDE, SQUARE_SIDE);
+    }
+
+    // Displaying row titles, starting from A
+    for(int i = 0, letter = 'A'; i < ROWS; ++i, ++letter)
+    {
+        QString letter_string = "";
+        letter_string.append(letter);
+        QLabel* label = new QLabel(letter_string, this);
+        label->setGeometry(LEFT_MARGIN - SQUARE_SIDE,
+                           TOP_MARGIN + i * SQUARE_SIDE,
+                           SQUARE_SIDE, SQUARE_SIDE);
+    }
+}
+
+void MainWindow::draw_fruit()
+{
+    // Vector of fruits
+    const std::vector<std::string>
+            fruits = {"cherries", "strawberry", "orange", "pear", "apple",
+                      "bananas", "tomato", "grapes", "eggplant"};
+
+    // Defining where the images can be found and what kind of images they are
+    const std::string PREFIX(":/");
+    const std::string SUFFIX(".png");
+
+    // Converting image (png) to a pixmap
+    int i = 0; // try different values in 0 <= i < fruits.size()
+    std::string filename = PREFIX + fruits.at(i) + SUFFIX;
+    QPixmap image(QString::fromStdString(filename));
+
+    // Scaling the pixmap
+    image = image.scaled(SQUARE_SIDE, SQUARE_SIDE);
+
+    // Setting the pixmap for a new label
+    QLabel* label = new QLabel("test", this);
+    label->setGeometry(LEFT_MARGIN + COLUMNS * SQUARE_SIDE,
+                       TOP_MARGIN + ROWS * SQUARE_SIDE,
+                       SQUARE_SIDE, SQUARE_SIDE);
+    label->setPixmap(image);
+}
