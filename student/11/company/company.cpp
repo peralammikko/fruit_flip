3e7a9f450fee8764ed5ef96d1b9e8cddbceb7193
Mikko Perälä <mikko.perala@tuni.fi>
2020-11-23 01:08:10 +0200

    Projekti 3: funktioiden kommentointi

--- student/11/company/company.cpp
+++ student/11/company/company.cpp
@@ -7,14 +7,18 @@ using namespace std;
 
 Company::Company() {}
 
+// Luokan destructor. Käy läpi jokaisen tietokannan jäsenen ja deletoi struct-rakenteen
+// (esimies-alaissuhten) ja poistaa sitten kyseisen mapin tietoparin.
 Company::~Company()
 {
-    for (auto iter : companyList) {
-        delete iter.second;
-        companyList.erase(iter.first);
+    map<string, Employee*>::iterator iter = companyList.begin();
+    if (iter != companyList.end()) {
+        delete iter->second;
+        companyList.erase(iter);
     }
 }
 
+// Etsii ja palauttaa tietokannasta osoittimen annettuun henkilöön (ID).
 Employee *Company::getPointer(const std::string &id) const
 {
     Employee* personPointer = nullptr;
@@ -26,18 +30,27 @@ Employee *Company::getPointer(const std::string &id) const
     return personPointer;
 }
 
+// Tulostaa virheviestin tilanteessa, jossa käyttäjän syöttämää henkilöä (ID) ei löydy tietokannasta.
 void Company::printNotFound(const string &id, ostream &output) const
 {
         output << "Error. " << id << " not found." << endl;
 }
 
+/* Luo uudelle henkilölle Emploee-rakenteen ja lisää henkilön ohjelman tietokantaan.
+ * Tarkistaa aluksi getPointer-funktion avulla, onko henkilö jo aiemmin lisätty.
+ * Mikäli henkilö on jo olemassa, tulostaa ohjelma virheviestin. Muussa tapauksessa
+ * varataan uusi dynaaminen muuttuja, jolle syötetään tarvittavat tiedot, jotka saadaan
+ * funktion parametreista. Lopulta lisätään uusi työntekijä firman map-rakenteella
+ * toimivaan tietokantaan avaimena nimi (id) ja arvona työntekijän tiedot (Employee-tietue).
+*/
+
 void Company::addNewEmployee(const std::string &id, const std::string &dep, const double &time, std::ostream &output)
 {
-    Employee* new_Person = new Employee;
-    Employee* checkPerson = getPointer(id);
-    if (checkPerson != nullptr) {
+    Employee* new_Person = getPointer(id);
+    if (new_Person != nullptr) {
         output << "Error. Employee already added." << endl;
     } else {
+        new_Person = new Employee{};
         new_Person->id_ = id;
         new_Person->department_ = dep;
         new_Person->time_in_service_ = time;
@@ -45,10 +58,18 @@ void Company::addNewEmployee(const std::string &id, const std::string &dep, cons
     }
 }
 
+/* Luodaan henkilöiden välille esihenkilö-alaissuhteita. Funktio tarkastaa ensin,
+ * löytyykö henkilö tietokannasta (ja tulostaa virheviestin jos ei löydy).
+ * Tämän jälkeen funktio tarkastaa parametreista, onko henkilölle määritelty esimiestä.
+ * Jos boss-parametri on tyhjä, merkitään että henkilöllä ei ole esimiestä.
+ * Muussa tapauksessa etsitään esimies tietokannasta getPointer-funktiolla
+ * (ja tulostetaan virheviesti jos esimiestä ei löydy). Tämän jälkeen alainen yhdistetään
+ * esimieheen ja subordinate esimiehen alaiseksi.
+*/
 void Company::addRelation(const std::string &subordinate, const std::string &boss, std::ostream &output)
 {
     Employee* idPerson = getPointer(subordinate);
-    if (idPerson == nullptr) {return;}
+    if (idPerson == nullptr) {printNotFound(subordinate, output); return;}
 
     if (boss == "") {
         idPerson->boss_ = nullptr;
@@ -60,6 +81,13 @@ void Company::addRelation(const std::string &subordinate, const std::string &bos
     }
 }
 
+/* Tulostaa säiliössä olevan datan aakkosjärjestyksessä.
+ * Parametrit:
+ * (1) id = käyttäjän syöttämä henkilö
+ * (2) group = henkilöön liittyvä ryhmä, jota käyttäjä haluaa tietää (alaiset/esimiehet/kolleegat tms.)
+ * (3) container = datasäiliö muotoa set<string>
+ * (4) output = tulostuskomento
+*/
 void Company::printGroup(const string &id, const string &group, const IdSet &container, ostream &output) const
 {
     int listLength = container.size();
@@ -74,6 +102,12 @@ void Company::printGroup(const string &id, const string &group, const IdSet &con
 
 }
 
+/* Siirtää vektorissa olevat yksitellen string-tyyppiset tiedot set-säiliöön,
+ * jonka jälkeen se palautuu funktioon aakkosjärjestyksessä.
+ * Käyttää header filessa määriteltyä IdSet -muotoa, joka on <set>string -säiliö.
+ *
+ * Parametri: container = struct-tietueita sisältävä vektori
+*/
 IdSet Company::VectorToIdSet(const std::vector<Employee *> &container) const
 {
     IdSet alphabeticalOrder = {};
@@ -84,17 +118,34 @@ IdSet Company::VectorToIdSet(const std::vector<Employee *> &container) const
 
 }
 
-void Company::listRecursive(const string &id, const int &depth, set<Employee*> &lista, int tempDepth = 0) const
+/* Hierarkian etsimiseen ja tallentamiseen käytettävä apufunktio.
+ * Etsii esimies-alaissuhteiden puusta pyydetyn henkilön ja listaa rekursiivisesti
+ * kaikki henkilön suorat ja epäsuorat alaiset set-säiliöön. Funktio kutsuu itseään
+ * uudestaan siis jokaisen alaisen kohdalla ja listaa alaisia niin kauan, kunnes alaisia ei
+ * enää löydy tai hierarkiataso n saavutetaan.
+ *
+ * Henkilötietojen osoittimet tallentuvat set-säiliöön, jolloin ne tallentuvat
+ * aakkosjärjestykseen, ja jotka myöhemmin palautuvat viitteenä alkuperäiseen funktioon.
+ * Parametrit:
+ * (1) id = käyttäjän syöttämän henkilön nimi
+ * (2) depth = käyttäjän toivoma hierarkiatason syvyys
+ * (3) list = lista henkilön id alaisten tiedoista
+ * (4) tempDepth = rekursiossa käytettävä apuparametri syvyyden saavuttamiseksi
+*/
+
+void Company::listRecursive(const string &id, const int &depth, set<Employee*> &list, int tempDepth = 0) const
 {
     Employee* idPerson = getPointer(id);
     if (tempDepth < depth) {
         for (auto sub : idPerson->subordinates_) {
-            lista.insert(sub);
-            listRecursive(sub->id_, depth, lista, tempDepth+1);
+            list.insert(sub);
+            listRecursive(sub->id_, depth, list, tempDepth+1);
         }
     }
 }
 
+
+// Tulostaa kaikkien tietokannassa olevien henkilöiden nimet, osastot ja palvelusajat.
 void Company::printEmployees(std::ostream &output) const
 {
     for (auto it : companyList) {
@@ -102,18 +153,31 @@ void Company::printEmployees(std::ostream &output) const
     }
 }
 
+
+/* Etsii ja tulostaa henkilön lähimmän esimiehen.
+ * Henkilö ja tämän esimies etsitään tietokannasta, jonka jälkeen
+ * tulostus tapahtuu printGroup -apufunktion avulla. Mikäli henkilöllä ei ole
+ * esimiestä, apufunktio tulostaa "<id> has no bosses."
+*/
 void Company::printBoss(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
-    if (idPerson == nullptr) {printNotFound(id,cout); return;}
+    if (idPerson == nullptr) {printNotFound(id, output); return;}
     Employee* idsBoss = idPerson->boss_;
-    set<string> bossId = {};
+    IdSet bossId = {};
     if (idsBoss != nullptr) {
         bossId.insert(idsBoss->id_);
     }
     printGroup(id,"bosses",bossId, output);
 }
 
+
+/* Etsii ja tulostaa henkilön lähimmät (suorat) alaiset.
+ * Henkilö etsitään tietokannasta ja tälle merkatut suorat alaiset tallennetaan vektoriin,
+ * jonka sisältö siirretään VectorToIdSet -apufunktion avulla aakkosjärjestykseen set-säiliöön
+ * tulostusta varten (jonka hoitaa apufunktio printGroup). Mikäli henkilöllä ei ole suoria
+ * alaisia, ohjelma tulostaa "<id> has no subordinates."
+*/
 void Company::printSubordinates(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
@@ -123,13 +187,21 @@ void Company::printSubordinates(const std::string &id, std::ostream &output) con
     printGroup(id, "subordinates", subSet, output);
 }
 
+
+/* Etsii ja tulostaa henkilön <id> kollegat eli saman
+ * esimiehen alla (ja samalla hierarkiatasolla) toimivat työntekijät.
+ * Etsii tietokannasta henkilön <id> esimiehen ja listaa tämän suorat alaiset
+ * IdSet-säiliöön (listaamatta itse henkilöä <id> omaksi kollegakseen), joka
+ * tulostetaan apufunktio printGroupin avulla. Jos henkilöllä ei ole esimiestä
+ * tai muita samantasoisia työntekijöitä, ohjelma tulostaa "<id> has no colleagues."
+*/
 void Company::printColleagues(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
     if (idPerson == nullptr) {printNotFound(id, cout); return;}
 
     Employee* idsBoss = idPerson->boss_;
-    set<string> colleagueList = {};
+    IdSet colleagueList = {};
     if (idsBoss != nullptr) {
         for (auto entity : idsBoss->subordinates_) {
             if (entity->id_ == id) {
@@ -141,14 +213,25 @@ void Company::printColleagues(const std::string &id, std::ostream &output) const
     printGroup(id, "colleagues", colleagueList, output);
 }
 
-
+/* Etsii ja tulostaa henkilön <id> osastolla olevat työntekijät.
+ * Funktio etsii ensin henkilön <id> korkeimman esimiehen, jolla on sama osasto kuin
+ * <id>:llä liikkumalla ylöspäin hierarkiassa. Korkeimman osaston esimiehen löydyttyä
+ * kutsutaan apufunktiota listRecursive, joka listaa kaikki kyseisen henkilön suorat
+ * ja epäsuorat alaiset, kunnes alaisia ei enää löydy.
+ *
+ * Funktioon palautuu lista kaikista alaisten tiedoista, joka sitten käydään
+ * läpi vertaamalla listan henkilöiden osastoa alkuperäisen henkilön
+ * <id> osastoon. Samalla osastolla työskentelevien nimet lisätään IdSet-säiliöön
+ * departNames, ja samoin itse <id> jos henkilö ei ole osastonsa korkein esimies.
+ * Säiliön tiedot tulostetaan apufunktion printGroup avulla.
+*/
 void Company::printDepartment(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
     if (idPerson == nullptr) {printNotFound(id,output); return;}
     Employee* idsBoss = idPerson->boss_;
     set<Employee*> lineList = {};
-    set<string> departNames = {};
+    IdSet departNames = {};
     while (idsBoss != nullptr) {
         if (idsBoss->department_ == idPerson->department_) {
             idPerson = idsBoss;
@@ -172,9 +255,15 @@ void Company::printDepartment(const std::string &id, std::ostream &output) const
         departNames.insert(i->id_);
     }
     printGroup(id, "department colleagues",departNames,output);
-
 }
 
+
+/* Tulostaa henkilön <id> linjaorganisaatiosta (henkilöstä alaspäin muodostuva hierarkia)
+ * pisimpään palvelleen henkilön nimen ja palvelusajan.
+ * Funktio ensin etsii linkitetyistä tietueista henkilön <id> kaikki alaiset listRecursive -apufunktion avulla.
+ * Apufunktio palauttaa listan kaikista suorista ja epäsuorista alaisista, ja alkaa vertailemaan
+ * työntekijöiden palvelusaikoja (alkuun vertailuarvoksi annettu <id>:n palvelusaika).
+*/
 void Company::printLongestTimeInLineManagement(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
@@ -198,6 +287,13 @@ void Company::printLongestTimeInLineManagement(const std::string &id, std::ostre
 
 }
 
+
+/* Tulostaa henkilön <id> linjaorganisaatiosta (henkilöstä alaspäin muodostuva hierarkia)
+ * lyhimpään palvelleen henkilön nimen ja palvelusajan.
+ * Funktio ensin etsii linkitetyistä tietyeista henkilön <id> kaikki alaiset listRecursive -apufunktion avulla.
+ * Apufunktio palauttaa listan kaikista suorista ja epäsuorista alaisista ja alkaa vertailemaan
+ * työntekijöiden palvelusaikoja (alkuun vertailuarvona <id>:n palvelusaika).
+*/
 void Company::printShortestTimeInLineManagement(const std::string &id, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
@@ -220,35 +316,55 @@ void Company::printShortestTimeInLineManagement(const std::string &id, std::ostr
     cout << "With the time of " << minTime << ", " << minTimePerson << " is the shortest-served employee in " << id << "'s line management." << endl;
 }
 
+
+/* Etsii ja tulostaa henkiön <id> kaikki esimiehet annetulle hierarkiatasolle n asti.
+ * Luo IdSet-säiliön henkilön esimiehiä varten, johon esimiehet voidaan tallentaa aakkosjärjestyksessä.
+ * Funktio etsii henkilön <id> esimiehen ja lisää esimiehen nimen säiliöön. Tämän jälkeen
+ * funktio siirtyy hierarkiassa yhden tason ylemmäksi ja etsii <id>:n esimiehen esimiehen jne
+ * kunnes ylempää esimiestä ei enää löydy tai taso n saavutetaan. Säiliön tiedot tulostetaan
+ * apufunktion printGroup avulla.
+ *
+ * Tulostaa virheviestin, mikäli henkilöä <id> ei löydy tai taso n on liian pieni (alle 1).
+*/
 void Company::printBossesN(const std::string &id, const int n, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
     if (idPerson == nullptr) {printNotFound(id,cout); return;}
     if (n < 1) {output << "Error. Level can't be less than 1." << endl; return;}
-    set<string> setBosses = {};
+    IdSet setBosses = {};
     int i = 0;
     while (i < n) {
-        if (idPerson->boss_ == nullptr) {break;}
-            string bossName = idPerson->boss_->id_;
-            setBosses.insert(bossName);
-            idPerson = idPerson->boss_;
-            i++;
+        if (idPerson->boss_ == nullptr) {
+            break;
+        }
+        string bossName = idPerson->boss_->id_;
+        setBosses.insert(bossName);
+        idPerson = idPerson->boss_;
+        i++;
     }
     printGroup(id, "bosses", setBosses, output);
 }
 
+
+/* Etsii ja tulostaa henkilön <id> kaikki alaiset annetulle hierarkiatasolle n asti.
+ * Funtio etsii ensin apufunktio listRecursiven avulla kaikki henkilön <id> suorat alaiset, jonka
+ * jälkeen apufunktio siirtyy yhden hierarkiatason alemmaksi ja jatkaa listaamista kunnes
+ * hierarkiataso n saavutetaan tai alaisia ei enää löydy. Apufunktio palauttaa siis listan kaikista
+ * henkilön <id> suorista ja epäsuorista alaisista, joiden nimet lisätään IdSet -säiliöön
+ * aakkosjärjestyksessä. Nimet tulostetaan apufunktion printGroup avulla.
+*/
 void Company::printSubordinatesN(const std::string &id, const int n, std::ostream &output) const
 {
     Employee* idPerson = getPointer(id);
     if (idPerson == nullptr) {printNotFound(id,cout); return;}
     if (n < 1) {output << "Error. Level can't be less than 1." << endl; return;}
     set<Employee*> setSubs = {};
-    set<string> setIds = {};
+    IdSet subIds = {};
     listRecursive(idPerson->id_, n, setSubs);
     for (auto i : setSubs) {
-        setIds.insert(i->id_);
+        subIds.insert(i->id_);
     }
-    printGroup(id,"subordinates",setIds,output);
+    printGroup(id,"subordinates",subIds,output);
 }
 
 
