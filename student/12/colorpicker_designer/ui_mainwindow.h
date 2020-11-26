6a8f8cd8d1ebeeb2530547e2955b3c44e8f52e2f
Mikko Perälä <mikko.perala@tuni.fi>
2020-11-26 14:17:58 +0200

    Colorpicker HD remake

--- student/12/colorpicker_designer/ui_mainwindow.h
+++ student/12/colorpicker_designer/ui_mainwindow.h
@@ -29,12 +29,12 @@ public:
     QWidget *centralWidget;
     QWidget *gridLayoutWidget;
     QGridLayout *gridLayout;
-    QLabel *labelBlue;
-    QLabel *labelRed;
-    QSlider *horizontalSliderBlue;
-    QLabel *labelGreen;
-    QSlider *horizontalSliderRed;
     QSlider *horizontalSliderGreen;
+    QSlider *horizontalSliderBlue;
+    QSlider *horizontalSliderRed;
+    QLabel *labelGreen;
+    QLabel *labelRed;
+    QLabel *labelBlue;
     QSpinBox *spinBoxRed;
     QSpinBox *spinBoxGreen;
     QSpinBox *spinBoxBlue;
@@ -52,21 +52,17 @@ public:
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         gridLayoutWidget = new QWidget(centralWidget);
         gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
-        gridLayoutWidget->setGeometry(QRect(50, 80, 231, 101));
+        gridLayoutWidget->setGeometry(QRect(50, 80, 241, 101));
         gridLayout = new QGridLayout(gridLayoutWidget);
         gridLayout->setSpacing(6);
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         gridLayout->setContentsMargins(0, 0, 0, 0);
-        labelBlue = new QLabel(gridLayoutWidget);
-        labelBlue->setObjectName(QString::fromUtf8("labelBlue"));
+        horizontalSliderGreen = new QSlider(gridLayoutWidget);
+        horizontalSliderGreen->setObjectName(QString::fromUtf8("horizontalSliderGreen"));
+        horizontalSliderGreen->setOrientation(Qt::Horizontal);
 
-        gridLayout->addWidget(labelBlue, 3, 0, 1, 1);
-
-        labelRed = new QLabel(gridLayoutWidget);
-        labelRed->setObjectName(QString::fromUtf8("labelRed"));
-
-        gridLayout->addWidget(labelRed, 1, 0, 1, 1);
+        gridLayout->addWidget(horizontalSliderGreen, 2, 1, 1, 1);
 
         horizontalSliderBlue = new QSlider(gridLayoutWidget);
         horizontalSliderBlue->setObjectName(QString::fromUtf8("horizontalSliderBlue"));
@@ -74,22 +70,26 @@ public:
 
         gridLayout->addWidget(horizontalSliderBlue, 3, 1, 1, 1);
 
-        labelGreen = new QLabel(gridLayoutWidget);
-        labelGreen->setObjectName(QString::fromUtf8("labelGreen"));
-
-        gridLayout->addWidget(labelGreen, 2, 0, 1, 1);
-
         horizontalSliderRed = new QSlider(gridLayoutWidget);
         horizontalSliderRed->setObjectName(QString::fromUtf8("horizontalSliderRed"));
         horizontalSliderRed->setOrientation(Qt::Horizontal);
 
         gridLayout->addWidget(horizontalSliderRed, 1, 1, 1, 1);
 
-        horizontalSliderGreen = new QSlider(gridLayoutWidget);
-        horizontalSliderGreen->setObjectName(QString::fromUtf8("horizontalSliderGreen"));
-        horizontalSliderGreen->setOrientation(Qt::Horizontal);
+        labelGreen = new QLabel(gridLayoutWidget);
+        labelGreen->setObjectName(QString::fromUtf8("labelGreen"));
 
-        gridLayout->addWidget(horizontalSliderGreen, 2, 1, 1, 1);
+        gridLayout->addWidget(labelGreen, 2, 0, 1, 1);
+
+        labelRed = new QLabel(gridLayoutWidget);
+        labelRed->setObjectName(QString::fromUtf8("labelRed"));
+
+        gridLayout->addWidget(labelRed, 1, 0, 1, 1);
+
+        labelBlue = new QLabel(gridLayoutWidget);
+        labelBlue->setObjectName(QString::fromUtf8("labelBlue"));
+
+        gridLayout->addWidget(labelBlue, 3, 0, 1, 1);
 
         spinBoxRed = new QSpinBox(gridLayoutWidget);
         spinBoxRed->setObjectName(QString::fromUtf8("spinBoxRed"));
@@ -132,9 +132,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        labelBlue->setText(QCoreApplication::translate("MainWindow", "blue:", nullptr));
-        labelRed->setText(QCoreApplication::translate("MainWindow", "red:", nullptr));
         labelGreen->setText(QCoreApplication::translate("MainWindow", "green:", nullptr));
+        labelRed->setText(QCoreApplication::translate("MainWindow", "red:", nullptr));
+        labelBlue->setText(QCoreApplication::translate("MainWindow", "blue:", nullptr));
         colorLabel->setText(QCoreApplication::translate("MainWindow", "TextLabel", nullptr));
     } // retranslateUi
 
